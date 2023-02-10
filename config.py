ab2354bc0050fe2c61b7ae0d2c298ca526d3e1ce
Keshava Tripathi <124826956+ShutupKeshav@users.noreply.github.com>
2023-02-10 19:31:05 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,27 @@
+
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Get it from my.telegram.org
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+## Get it from @Botfather in Telegram.
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+# Database to save your chats and stats...
+MONGO_DB_URI = getenv("MONGO_DB_URI", "")
+
+# SUDO USERS
+SUDO_USER = list(
+    map(int, getenv("SUDO_USER", "").split())
+)  # Input type must be interger
+
+# START IMAGE 
+START_IMAGE = getenv("START_IMAGE", "")
+
+# WELCOME IMAGE
+WELCOME_IMAGE = getenv("WELCOME_IMAGE", "")
